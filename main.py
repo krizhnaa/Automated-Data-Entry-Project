a499665205afc3bcb6f8fa8d25451bc5d20b109e
krizhnaa <krizhnaprem@gmail.com>
2024-01-26 09:09:46 +0530

    Done with the project

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ response = requests.get("https://appbrewery.github.io/Zillow-Clone/")
 property_page = response.text
 soup = BeautifulSoup(property_page, "html.parser")
 
-prettified_html = soup.prettify()
-print(prettified_html)
+# prettified_html = soup.prettify()
+# print(prettified_html)
 
 property_prices_element = soup.find_all('span', class_='PropertyCardWrapper__StyledPriceLine')
 property_prices = []
 
 for price in property_prices_element:
-    price = int(price.get_text().split("+")[0].split("/")[0].split("$")[1].replace(",",""))
+    price = price.get_text().split("+")[0].split("/")[0].replace(",","")
     property_prices.append(price)
 
 property_addr_element = soup.find_all('address', {'data-test': 'property-card-addr'})
@@ -35,7 +35,7 @@ driver=webdriver.Chrome(options=chrome_options)
 
 property_details = zip(property_addrs, property_prices, property_links)
 
-for i in range(len(property_prices))
+for property in property_details:
     driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeWxsE4Hjn0vMY1e3tkRLoN3IrczUCVCXZxVcJrs-q_W4GKFw/viewform?usp=sf_link")
     time.sleep(0.15)
     addr_field = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
@@ -44,11 +44,19 @@ for i in range(len(property_prices))
 
     submit_btn = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
 
+    # print(property[1])
     addr_field.clear()
-    addr_field.send_keys("heyyy")
+    addr_field.send_keys(property[0])
+    time.sleep(0.1)
+    price_field.clear()
+    price_field.send_keys(property[1])
+    time.sleep(0.1)
+    link_field.clear()
+    link_field.send_keys(property[2])
+    time.sleep(0.1)
     submit_btn.click()
 
-    driver.quit()
+driver.quit()
 
 
 
