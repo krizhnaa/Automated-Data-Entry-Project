4c50fd0fe446f1a2ac6abda3118b5f4e15e3ab80
krizhnaa <krizhnaprem@gmail.com>
2024-01-26 08:01:09 +0530

    Scarped Property Addresses, prices and links and appended it to a list

--- main.py
+++ main.py
@@ -10,11 +10,42 @@ soup = BeautifulSoup(property_page, "html.parser")
 # prettified_html = soup.prettify()
 # print(prettified_html)
 
-property_prices = soup.find_all('span', class_='PropertyCardWrapper__StyledPriceLine')
+property_prices_element = soup.find_all('span', class_='PropertyCardWrapper__StyledPriceLine')
+property_prices = []
 
-for price in property_prices:
+for price in property_prices_element:
     price = int(price.get_text().split("+")[0].split("/")[0].split("$")[1].replace(",",""))
-    print(price)
+    property_prices.append(price)
+
+property_addr_element = soup.find_all('address', {'data-test': 'property-card-addr'})
+property_addrs = []
+for addr in property_addr_element:
+    property_addrs.append(addr.get_text().strip())
+
+property_anchor_element = soup.find_all('a', {'data-test': 'property-card-link'})
+property_links = []
+for anchor in property_anchor_element:
+    property_links.append(anchor.get("href"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # chrome_options = webdriver.ChromeOptions()
